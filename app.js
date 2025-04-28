ce7b93bc969136b92b298ff5563744a83db59f60
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2025-04-28 18:58:43 +0500

    #3. Database & env

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const postModel = require('./models/post.mode')
 
 const app = express()
 
-const PORT = 8080
+app.use(express.json())
 
-app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
+app.get('/', async (req, res) => {
+	try {
+		const allPost = await postModel.find()
+		res.status(200).json(allPost)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+app.post('/', async (req, res) => {
+	try {
+		const { title, body } = req.body
+		const newPost = await postModel.create({ title, body })
+		res.status(201).json(newPost)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+app.delete('/:id', (req, res) => {
+	const { id } = req.params
+	res.send(id)
+})
+
+app.put('/:id', (req, res) => {
+	const { id } = req.params
+	const body = req.body
+
+	res.json({ id, body })
+})
+
+const PORT = process.env.PORT || 8080
+
+const botstrap = async () => {
+	try {
+		await mongoose
+			.connect(process.env.DB_URL)
+			.then(() => console.log('Connected DB'))
+		app.listen(PORT, () =>
+			console.log(`Listening on - http://localhost:${PORT}`)
+		)
+	} catch (error) {
+		console.log(`Error connecting with DB:${error} `)
+	}
+}
+
+botstrap()
